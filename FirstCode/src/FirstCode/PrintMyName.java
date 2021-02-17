f04a34e89f17f7d18ee419df5e84e295f8f4fd1f
يحيى <@DESKTOP-RBAQU6P>
2021-02-17 11:55:19 +0300

    First commit

---
+++ FirstCode/src/FirstCode/PrintMyName.java
@@ -0,0 +1,10 @@
+package FirstCode;
+public class PrintMyName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println ("my name is yahya Alnajrani");
+
+	}
+
+}
